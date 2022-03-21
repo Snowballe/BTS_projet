6bd68504136de65182b98a61167957f416a3d204
Snowballe <62845660+Snowballe@users.noreply.github.com>
2022-03-21 13:37:06 +0100

    adding dto for propositions

--- RaminagrobisDTO/DTO/Propositions_DTO.cs
+++ RaminagrobisDTO/DTO/Propositions_DTO.cs
@@ -8,8 +8,10 @@ namespace Raminagrobis.DTO.DTO
 {
     public class Propositions_DTO
     {
+        public int ID { get; set; }
         public int Prix { get; set; }
         public int ID_ligne_global { get; set; }
         public int ID_fournisseur { get; set; }
+        public int ID_produit { get; set; }
     }
 }
